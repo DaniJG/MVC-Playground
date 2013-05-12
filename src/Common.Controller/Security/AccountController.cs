72b876c24b2b595b380d7426fbf867a5460bc8fb
Daniel <danieljimgarcia@gmail.com>
2013-05-12 14:30:20 +0100

    Refactored profile managament action and views

--- src/Common.Controller/Security/AccountController.cs
+++ src/Common.Controller/Security/AccountController.cs
@@ -112,62 +112,59 @@ namespace DJRM.Common.Controllers.Security
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Manage(LocalPasswordModel model)
+        public ActionResult ChangePassword(LocalPasswordModel model)
         {
-            bool hasLocalAccount = OAuthWebSecurity.HasLocalAccount(WebSecurity.GetUserId(User.Identity.Name));
-            ViewBag.HasLocalPassword = hasLocalAccount;
-            ViewBag.ReturnUrl = Url.Action("Manage");
-            if (hasLocalAccount)
+            if (ModelState.IsValid)
             {
-                if (ModelState.IsValid)
+                // ChangePassword will throw an exception rather than return false in certain failure scenarios.
+                bool changePasswordSucceeded;
+                try
                 {
-                    // ChangePassword will throw an exception rather than return false in certain failure scenarios.
-                    bool changePasswordSucceeded;
-                    try
-                    {
-                        changePasswordSucceeded = WebSecurity.ChangePassword(User.Identity.Name, model.OldPassword, model.NewPassword);
-                    }
-                    catch (Exception)
-                    {
-                        changePasswordSucceeded = false;
-                    }
-
-                    if (changePasswordSucceeded)
-                    {
-                        return RedirectToAction("Manage", new { Message = ManageMessageId.ChangePasswordSuccess });
-                    }
-                    else
-                    {
-                        ModelState.AddModelError("", "The current password is incorrect or the new password is invalid.");
-                    }
+                    changePasswordSucceeded = WebSecurity.ChangePassword(User.Identity.Name, model.OldPassword, model.NewPassword);
                 }
-            }
-            else
-            {
-                // User does not have a local password so remove any validation errors caused by a missing
-                // OldPassword field
-                ModelState state = ModelState["OldPassword"];
-                if (state != null)
+                catch (Exception)
                 {
-                    state.Errors.Clear();
+                    changePasswordSucceeded = false;
                 }
 
-                if (ModelState.IsValid)
-                {
-                    try
-                    {
-                        WebSecurity.CreateAccount(User.Identity.Name, model.NewPassword);
-                        return RedirectToAction("Manage", new { Message = ManageMessageId.SetPasswordSuccess });
-                    }
-                    catch (Exception e)
-                    {
-                        ModelState.AddModelError("", e);
-                    }
+                if (!changePasswordSucceeded)
+                {                    
+                    ModelState.AddModelError("", "The current password is incorrect or the new password is invalid.");
                 }
             }
+                  
 
             // If we got this far, something failed, redisplay form
-            return View(model);
+            return PartialView("_ChangePasswordPartial", model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SetPassword(LocalPasswordModel model)
+        {            
+            // User does not have a local password so remove any validation errors caused by a missing
+            // OldPassword field
+            ModelState state = ModelState["OldPassword"];
+            if (state != null)
+            {
+                state.Errors.Clear();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    WebSecurity.CreateAccount(User.Identity.Name, model.NewPassword);
+                    return PartialView("_ChangePasswordPartial", model);
+                }
+                catch (Exception e)
+                {
+                    ModelState.AddModelError("", e);
+                }
+            }
+            
+            // If we got this far, something failed, redisplay form
+            return PartialView("_SetPasswordPartial", model);
         }
 
         //
